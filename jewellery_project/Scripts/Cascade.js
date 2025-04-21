67e2e0926d17434995eeac977cd6ab8faa928eed
Amisha Patel <32287860+amisha7@users.noreply.github.com>
2025-04-21 16:43:46 -0400

    Add files via upload

---
+++ jewellery_project/Scripts/Cascade.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+    $("#ddlst").change(function () {
+        $.ajax({
+            type: 'post',
+            url: "/admin/getproduct",
+            data: { stid: $('#ddlst').val() },
+            datatype: 'json',
+            traditional: true,
+            success: (function (ls) {
+                var d = "<select id='ddlct'>";
+                d = d + "<option value=''>--Select Product--</option>";
+
+                for (var i = 0; i < ls.length; i++) {
+
+                    d = d + '<option value=' + ls[i].Value + '>' + ls[i].Text + '</option>';
+                }
+                d = d + '</select>';
+                $('#ddlct').html(d);
+            })
+
+        })
+    })
+})
+
+
+//$(document).ready(function () {
+//    $("#ddlct").change(function () {
+//        $.ajax({
+//            type: 'post',
+//            url: "/DropDown/getPincode",
+//            data: { ctid: $('#ddlct').val() },
+//            datatype: 'json',
+//            traditional: true,
+//            success: (function (ls) {
+//                var d = "<select id='ddlpin'>";
+//                d = d + "<option value=''>Select City</option>";
+
+//                for (var i = 0; i < ls.length; i++) {
+
+//                    d = d + '<option value=' + ls[i].Value + '>' + ls[i].Text + '</option>';
+//                }
+//                d = d + '</select>';
+//                $('#ddlpin').html(d);
+//            })
+
+//        })
+//    })
+//})
---
+++ jewellery_project/Scripts/Cascade1.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $("#ddlst").change(function () {
+        $.ajax({
+            type: 'post',
+            url: "/User_new/getcity",
+            data: { stid: $('#ddlst').val() },
+            datatype: 'json',
+            traditional: true,
+            success: (function (ls) {
+                var d = "<select id='ddlct'>";
+                d = d + "<option value=''>--Select City--</option>";
+
+                for (var i = 0; i < ls.length; i++) {
+
+                    d = d + '<option value=' + ls[i].Value + '>' + ls[i].Text + '</option>';
+                }
+                d = d + '</select>';
+                $('#ddlct').html(d);
+            })
+
+        })
+    })
+})
