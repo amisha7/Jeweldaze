53ab16c6423ede61221277139439b127d821c222
Amisha Patel <32287860+amisha7@users.noreply.github.com>
2025-04-21 16:44:21 -0400

    Add files via upload

---
+++ jewellery_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jewellery_project.Startup))]
+namespace jewellery_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
