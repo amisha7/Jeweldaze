4ec5334766127dc311d86a59d720e56ed27a56b8
Amisha Patel <32287860+amisha7@users.noreply.github.com>
2025-04-21 15:35:28 -0400

    Add files via upload

---
+++ jewellery_project/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace jewellery_project
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
